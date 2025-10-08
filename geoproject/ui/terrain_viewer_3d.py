02388d26e6d8914694b4e36e2a910aba703a5ad3
SHIVAGANESHKING <SHIVAGANESHKING@GMAIL.COM>
2025-10-08 13:13:34 +0530

    Update terrain_viewer_3d.py

--- geoproject/ui/terrain_viewer_3d.py
+++ geoproject/ui/terrain_viewer_3d.py
@@ -1,5 +1,5 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
-from PyQt6.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt5.QtCore import Qt
 
 class TerrainViewer3D(QWidget):
     """A placeholder widget for the 3D terrain viewer."""
